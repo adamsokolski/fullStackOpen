61524dbf83e2e8c6d98682f6b596ee9f5c37c0da
Adam Sokólski <idKrazu@gmail.com>
2022-05-23 20:21:40 +0200

    ✨ React component state practise

---
+++ course_contents/part1/comp_state/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
+    </>
+  )
+}
+
+export default App
