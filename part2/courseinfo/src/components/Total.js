32cddeeb4247985546756665ff894c69ea0cf42c
Adam Sokólski <idKrazu@gmail.com>
2022-06-05 17:37:37 +0200

    ✨ Exercises 2.3 - 2.5

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Total = ({ course }) => {
-  let total = 0
-  course.parts.forEach((e) => (total += e.exercises))
+  let total = course.parts.reduce((s, p) => {
+    // first s is a object
+    if (s.exercises) {
+      return s.exercises + p.exercises
+    }
+    return s + p.exercises
+  })
   return <strong>total of {total} exercises</strong>
 }
 
